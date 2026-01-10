5157930d59d784bc8e22c87ea44b97fc07b60c55
ikermartfer <iker.martfer@gmail.com>
2026-01-10 20:19:28 +0100

    Finished logic gates

--- main.py
+++ main.py
@@ -6,20 +6,26 @@ SERIES=lambda input1: lambda input2: input1 and input2
 MIDDLE=lambda input1: lambda input2: input1 or input2
 #Translation from True and False to one and zero for debugging
 NUM=lambda val: "ONE" if val else "ZERO"
+#Table for debugging
+def table(fn):
+    print(f"ZERO ZERO = {fn(ZERO)(ZERO)}")
+    print(f"ONE ZERO = {fn(ONE)(ZERO)}")
+    print(f"ZERO ONE = {fn(ZERO)(ONE)}")
+    print(f"ONE ONE = {fn(ONE)(ONE)}")
 #For clarity
 VOLTAGE=True
 GROUND=False
 ONE=True
 ZERO=False
 #Logic Gates (remaining: XOR,NXOR)
-NOT=lambda input: (
+NOT=lambda input: (         #2 transistors
     MIDDLE(
         N(input)(GROUND)
     )(
         P(input)(VOLTAGE)
     )
 )
-AND=lambda input1: lambda input2: (
+AND=lambda input1: lambda input2: (         #4 transistors
     MIDDLE(
         SERIES(
             P(input1)(GROUND)
@@ -34,8 +40,8 @@ AND=lambda input1: lambda input2: (
         )
     )
 )
-NAND= lambda input1: lambda input2: NOT(AND(input1)(input2))
-NOR=lambda input1: lambda input2: (
+NAND= lambda input1: lambda input2: NOT(AND(input1)(input2))        #6 transistors
+NOR=lambda input1: lambda input2: (         #4 transistors
     MIDDLE(
         SERIES(
             P(input1)(VOLTAGE)
@@ -50,5 +56,26 @@ NOR=lambda input1: lambda input2: (
         )
     )
 )
-OR=lambda input1: lambda input2: NOT(NOR(input1)(input2))
+OR=lambda input1: lambda input2: NOT(NOR(input1)(input2))           #6 transistors
+NXOR=lambda input1: lambda input2: (            #10 transistors
+    MIDDLE(
+        OR(
+            SERIES(
+                P(input1)(VOLTAGE)
+            )(
+                P(input2)(VOLTAGE)
+            )
+        )(
+            SERIES(
+                N(input1)(VOLTAGE)
+            )(
+                N(input2)(VOLTAGE)
+            )
+        )
+    )(
+        GROUND
+    )
+)
+XOR=lambda input1: lambda input2: NOT(NXOR(input1)(input2))         #12 transistors
 
+table(XOR)
